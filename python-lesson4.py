349d53fbb2f0a8c791e5f6bd1ad8e907bb978de9
Aizat Ryspaeva <aizatryspaeva@gmail.com>
2024-10-30 20:40:31 -0400

    Python-day2

---
+++ python-lesson4.py
@@ -0,0 +1,9 @@
+for i in range(1,16):
+    if i == 3:
+        print("Skipping number 3")
+        continue
+    if i == 9:
+        print("Stopping number 9")
+        break
+    print("Current number is: ", i)
+      
